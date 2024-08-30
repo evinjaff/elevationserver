0ed50aa5bae9dcc55f182f1df77a8e0506c974b5
Evin Jaff <evin@jaffboys.com>
2024-08-30 11:24:13 -0500

    update to be pin-based

--- Config.py
+++ Config.py
@@ -1,10 +1,10 @@
 
 # GPIO pins for Pi3
 GPIO_LOOKUP = {
-    "preset1": 6,
-    "preset2": 13,
-    "preset3": 19,
-    "preset4": 26,
-    "up": 0,
-    "down": 5,
+    "preset1": 31,
+    "preset2": 33,
+    "preset3": 35,
+    "preset4": 37,
+    "up": 27,
+    "down": 29,
 }
